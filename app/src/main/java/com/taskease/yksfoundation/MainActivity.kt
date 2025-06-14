4b1b439959c116c937ea332c148861a80f951d9b
guruprasad <guruprasad1736@gmail.com>
2025-06-14 12:56:02 +0530

    Corrected the functionality of the Notification

--- app/src/main/java/com/taskease/yksfoundation/MainActivity.kt
+++ app/src/main/java/com/taskease/yksfoundation/MainActivity.kt
@@ -32,6 +32,12 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+
+        SharedPreferenceManager.init(this@MainActivity)
+
+        SharedPreferenceManager.removeKey(SharedPreferenceManager.NOTIFICATION_TOKEN)
+
+
         FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 val token = task.result
@@ -56,7 +62,6 @@ class MainActivity : AppCompatActivity() {
             }
 
 
-        SharedPreferenceManager.init(this@MainActivity)
 
         PermissionX.init(this)
             .permissions(
